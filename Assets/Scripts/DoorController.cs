33c16040faf13202d4b5ce64c4822cdfe2983fb7
Thomas Chang <thomaschangnl@gmail.com>
2021-03-18 23:27:59 -0400

    door

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Animator doorAnimator;
+    private bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnimator = transform.parent.GetComponent<Animator>();
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen)
+		{
+            doorAnimator.SetBool("doorState", true);
+		}
+        else
+		{
+            doorAnimator.SetBool("doorState", false);
+        }
+        
+    }
+
+    public void toggleDoor() => isOpen = isOpen ? false : true;
+}
---
+++ Assets/Scripts/InteractableController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableController : MonoBehaviour
+{
+    KeyCode interactKey = KeyCode.E;
+    private int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 2))
+        {
+            if (hit.collider.CompareTag("Door"))
+            {
+                if (Input.GetKeyDown(interactKey))
+                {
+                    GameObject door = hit.collider.gameObject;
+                    Debug.Log(counter++);
+                    door.GetComponent<DoorController>().toggleDoor();
+                }
+            }
+        }
+    }
+}
