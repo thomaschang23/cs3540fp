9e4af92b2cf0ec17efbddb0cc959776d7cd40c1e
Griffin J Rademacher <contact@griffin-rademacher.info>
2021-03-21 13:11:24 -0400

    Updates dialogue UI
    Adds new colorscheme to dialogue UI. Also implements a default name for a given conversation. Additionally, unifies the UI prompts with the buttons.

--- Assets/Dialogue/Scripts/DialogueTrigger.cs
+++ Assets/Dialogue/Scripts/DialogueTrigger.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour {
-  public Dialogue dialogue;
+public class DialogueTrigger : MonoBehaviour
+{
+    public string defaultName;
+    public Dialogue dialogue;
 
-  public void TriggerDialogue() {
-    FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-  }
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue, defaultName);
+    }
 }
