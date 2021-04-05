343a1a0e445f52336b86b20b2ce696498073d20a
Griffin J Rademacher <contact@griffin-rademacher.info>
2021-04-04 23:14:30 -0400

    adds UI for case note controls

--- Assets/UI/Scripts/UIManager.cs
+++ Assets/UI/Scripts/UIManager.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public Animator notesAnimator;
     public Animator dialogueAnimator;
+    public Image notesIcon;
+    public Image notesIconBkgd;
+    public Text notesIconText;
+    public Color notesIconInactive;
+    public Color notesIconBkgdInactive;
+    public Color notesIconTextInactive;
+    public Color notesIconActive;
+    public Color notesIconBkgdActive;
+    public Color notesIconTextActive;
 
     private bool areNotesOpen = false;
     private bool isDialogueOpen = false;
@@ -14,11 +24,17 @@ public class UIManager : MonoBehaviour
     {
         if (areNotesOpen)
         {
+            notesIcon.color = notesIconInactive;
+            notesIconBkgd.color = notesIconBkgdInactive;
+            notesIconText.color = notesIconTextInactive;
             areNotesOpen = false;
             notesAnimator.SetBool("IsOpen", false);
         }
         else
         {
+            notesIcon.color = notesIconActive;
+            notesIconBkgd.color = notesIconBkgdActive;
+            notesIconText.color = notesIconTextActive;
             areNotesOpen = true;
             notesAnimator.SetBool("IsOpen", true);
         }
