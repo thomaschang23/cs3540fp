a5079f13f21eca7f7632ebb3102e9d7013c85e19
Thomas Chang <thomaschangnl@gmail.com>
2021-04-23 14:48:06 -0400

    Include examine in InteractableController

--- Assets/Scripts/InteractableController.cs
+++ Assets/Scripts/InteractableController.cs
@@ -16,14 +16,24 @@ public class InteractableController : MonoBehaviour
     private Text pickUpText;
     private Text doorText;
 
+    private Text examineText;
+    private GameObject examineObject;
+    private bool examining;
+    private UIManager ui;
+    Vector3 origPosition;
+    Vector3 origRotation;
+
     private void Start()
 	{
         pickUpText = GameObject.FindGameObjectWithTag("PickUpPopUpText").GetComponent<Text>();
         doorText = GameObject.FindGameObjectWithTag("DoorPopUpText").GetComponent<Text>();
+        examineText = GameObject.FindGameObjectWithTag("ExaminePopUpText").GetComponent<Text>();
+        ui = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
     }
 
 	void Update()
     {
+        updateExamine();
         updatePickup();
         updateDoor();
     }
@@ -51,6 +61,64 @@ public class InteractableController : MonoBehaviour
         {
             if (doorText != null)
                 doorText.text = "";
+        }
+    }
+
+    private void updateExamine()
+    {
+        RaycastHit hit;
+
+        if (examining)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                float rotSpeed = 15;
+
+                float xAxis = Input.GetAxis("Mouse X") * rotSpeed;
+                float yAxis = Input.GetAxis("Mouse Y") * rotSpeed;
+
+                examineObject.transform.Rotate(Vector3.up, -xAxis, Space.World);
+                examineObject.transform.Rotate(Vector3.right, yAxis, Space.World);
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                examineText.text = "";
+                examining = ui.ToggleExamine();
+                examineObject.transform.position = origPosition;
+                examineObject.transform.eulerAngles = origRotation;
+
+                Time.timeScale = 1;
+
+                examining = false;
+            }
+        }
+        else
+		{
+            if (Physics.Raycast(transform.position, transform.forward, out hit, interactableDistance))
+			{
+                if (hit.collider.CompareTag("Examine"))
+                {
+                    examineText.text = "Click to Examine";
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        examining = ui.ToggleExamine();
+                        examineText.text = "Right Click to Exit";
+                        examineObject = hit.transform.gameObject;
+
+                        origPosition = examineObject.transform.position;
+                        origRotation = examineObject.transform.rotation.eulerAngles;
+
+                        examineObject.transform.position = gameObject.transform.position + (gameObject.transform.forward);
+
+                        Time.timeScale = 0;
+                    }
+                }
+            }
+            else
+			{
+                examineText.text = "";
+
+            }
         }
     }
 
@@ -85,6 +153,8 @@ public class InteractableController : MonoBehaviour
                         heldObject.GetComponent<Rigidbody>().isKinematic = true;
                     }
                 }
+
+                
             }
             else
             {
